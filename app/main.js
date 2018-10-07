b2db4f9d5679335585931e4a24c86547e033d025
Thiago A. Hagy <thiagohagy@hotmail.com>
2018-10-07 14:01:13 -0300

    test

--- app/main.js
+++ app/main.js
@@ -13,7 +13,10 @@ if(TNS_ENV !== 'production') {
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
 
-Vue.registerElement('RadSideDrawer', () => require('nativescript-ui-sidedrawer').RadSideDrawer)
+// Vue.registerElement('RadSideDrawer', () => require('nativescript-ui-sidedrawer').RadSideDrawer)
+import RadSideDrawer from "nativescript-ui-sidedrawer";
+
+Vue.use(RadSideDrawer);
 
 new Vue({
   render: h => h('frame', [h(App)])
